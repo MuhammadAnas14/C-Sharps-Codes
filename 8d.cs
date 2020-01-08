b84ba54454ca53553be6843b38c720d8c8209b2d
Anas452 <anasm9877@gmail.com>
2020-01-08 12:39:31 +0500

    lab8 codes

---
+++ 8d.cs
@@ -0,0 +1,53 @@
+
+twitter
+linkedIn
+Reddit
+expand
+Download Free .NET & JAVA Files API
+Try Free File Format APIs for Word/Excel/PDF
+
+using System;  
+class Program  
+{  
+    static void Main()  
+    {  
+        Console.Write("Enter first number : ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter third number : ");
+        c = Convert.ToInt32(Console.ReadLine());
+        sq(a,b,c);
+        //Waiting for output
+        Console.ReadKey();  
+    }  
+    public int sq(int x,int y,int z){
+                
+
+        int i;  
+        int [] a = new int[5]  {x,y,z};  
+        
+          
+        //Sorting the values  
+        for (i = 1; i <= n; i++)  
+        {  
+            for (int j = 1; j <= n - 1; j++)  
+            {  
+                if (a[j] > a[j + 1])  
+                {  
+                    int temp = a[j];  
+                    a[j] = a[j + 1];  
+                    a[j + 1] = temp;  
+                }  
+            }  
+        }  
+        Console.Write("Sorted number : ");  
+        for (i = 1; i <= n; i++)  
+        {  
+            Console.Write(a[i]+" ");
+        }
+
+    }
+    
+        
+}
---
+++ 8e.cs
@@ -0,0 +1,40 @@
+using System;  
+class Program  
+{  
+    static void Main()  
+    {  
+        int i;  
+        int[] a = new int[30]; // Array Declaration in C#  
+        Console.Write("Enter the Number of values to be Sort : ");  
+        // read the string value (by default) and convert it in to integer  
+        int n = Convert.ToInt16(Console.ReadLine());   
+        //Reading the values one by one  
+        for (i = 1; i <= n; i++)  
+        {  
+            Console.Write("Enter the No " + i + ":");  
+            // read the string value (by default) and convert it in to integer  
+            a[i] = Convert.ToInt16(Console.ReadLine());   
+        }  
+        //Sorting the values  
+        for (i = 1; i <= n; i++)  
+        {  
+            for (int j = 1; j <= n - 1; j++)  
+            {  
+                if (a[j] > a[j + 1])  
+                {  
+                    int temp = a[j];  
+                    a[j] = a[j + 1];  
+                    a[j + 1] = temp;  
+                }  
+            }  
+        }  
+        //Display the Ascending values one by one  
+        Console.Write("Ascending Sort : ");  
+        for (i = 1; i <= n; i++)  
+        {  
+            Console.Write(a[i]+" ");
+        }
+        //Waiting for output
+        Console.ReadKey();  
+    }  
+}
